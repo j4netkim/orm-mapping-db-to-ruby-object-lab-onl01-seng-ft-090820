80037ea3c3a033f946310a327a290a24c5bd02e4
J K <jkim65@buffalo.edu>
2020-10-10 19:36:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,7 +51,7 @@ class Student
     WHERE students.grade = 10
     LIMIT ?
     SQL
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql, x).map do |row|
       self.new_from_db(row)
     end
   end
