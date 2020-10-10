9535a3cb4be1b26746fb88d48a564c2ddacdd387
J K <jkim65@buffalo.edu>
2020-10-10 19:36:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,6 +44,7 @@ class Student
     end
   end
 
+
   def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
     SELECT *
@@ -55,6 +56,9 @@ class Student
       self.new_from_db(row)
     end
   end
+
+
+  
 
 
   def self.find_by_name(name)
