e6a4aa560e853f78c9039e0580308eb46db4d280
J K <jkim65@buffalo.edu>
2020-10-10 19:42:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -74,7 +74,12 @@ class Student
   def self.all_students_in_grade_X(x)
     sql = <<-SQL
     SELECT *
+    FROM students 
+    WHERE grade = ?
     SQL
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end
   end
 
 
