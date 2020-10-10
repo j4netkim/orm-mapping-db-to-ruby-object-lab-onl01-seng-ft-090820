df5787acae8d0b0bc292287bd31a2fc5a6043d20
J K <jkim65@buffalo.edu>
2020-10-10 18:47:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,6 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    sql = <<-SQL
     new_student = self.new
     new_student.id = row[0]
     new_student.name = row[1]
